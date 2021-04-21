58ef64c359902e07f583bf3669b506148e1b21bd
unknown <manassingh5>
2021-04-22 03:30:58 +0530

    Added Promotion Controller and SessionHelper classes

---
+++ PromotionEngineNetCore/Entities/Promotion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionEngineNetCore.Entities
+{
+    public class Promotion
+    {
+        public int PromotionId { get; set; }
+        public string ItemName { get; set; }
+        public int ItemQuantity { get; set; }
+        public decimal ItemPrice { get; set; }
+    }
+}
---
+++ PromotionEngineNetCore/Models/ShoppingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionEngineNetCore.Models
+{
+    public class ShoppingViewModel
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+        public string ItemImage { get; set; }
+        public decimal ItemPrice { get; set; }
+        public string Description { get; set; }
+    }
+}
