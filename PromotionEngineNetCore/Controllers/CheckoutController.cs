98a3ad792826be241ed5f30f7ac6b5c8320f8869
unknown <manassingh5>
2021-04-22 11:24:39 +0530

    Added Checkout Controller and its view, Added View for shopping cart

---
+++ PromotionEngineNetCore/Controllers/CheckoutController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PromotionEngineNetCore.Entities;
+using PromotionEngineNetCore.Helpers;
+using PromotionEngineNetCore.Models;
+
+namespace PromotionEngineNetCore.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<CheckoutModel> listBill = new List<CheckoutModel>();
+            List<ShoppingCartModel> cart = SessionHelper.GetObjectFromJson<List<ShoppingCartModel>>(HttpContext.Session, "cartdata");
+            List<Promotion> listPromotion = SessionHelper.GetObjectFromJson<List<Promotion>>(HttpContext.Session, "promotiondata");
+            int counter = 0;
+            if (listPromotion.Any(x => x.ItemName.Contains("&")))
+            {
+                var listPromotionItemName = listPromotion.Where(x => x.ItemName.Contains("&")).Select(x => new { CombinedItemName = x.ItemName, CombinedItemPrice = x.ItemPrice, CombinedQuantity = x.ItemQuantity }).FirstOrDefault();
+                var splitName = listPromotionItemName.CombinedItemName.Split('&');
+                var combiledtotalPrice = listPromotionItemName.CombinedItemPrice;
+                foreach (var name in splitName)
+                {
+                    if (cart.Any(x => x.ItemName == name))
+                    {
+                        counter++;
+                    }
+                }
+                if (counter >= 2)
+                {
+                    listBill.Add(new CheckoutModel() { ItemName = listPromotionItemName.CombinedItemName, TotalPrice = combiledtotalPrice, TotalQuantity = listPromotionItemName.CombinedQuantity });
+                }
+                else
+                {
+                    var element = cart.FirstOrDefault(x => x.ItemName == splitName[0]);
+                    listBill.Add(new CheckoutModel() { ItemName = element.ItemName, TotalPrice = element.ItemPrice, TotalQuantity = element.ItemQuantity });
+                }
+            }
+
+            var data = from a in listPromotion join b in cart on a.ItemName equals b.ItemName select new { promiq = a.ItemQuantity, promPrice = a.ItemPrice, b.ItemName, b.ItemQuantity, b.ItemPrice };
+            foreach (var item in data)
+            {
+                string name = item.ItemName;
+                int quantity = item.ItemQuantity;
+                int promQuantity = item.promiq;
+                decimal totalPrice = 0;
+                int totalQuantity = 0;
+                while (quantity > promQuantity)
+                {
+                    totalPrice = totalPrice + item.promPrice;
+                    promQuantity = promQuantity + promQuantity;
+                }
+
+                totalPrice = totalPrice + item.ItemPrice;
+                totalQuantity = quantity;
+
+                listBill.Add(new CheckoutModel() { ItemName = name, TotalPrice = totalPrice, TotalQuantity = totalQuantity });
+            }
+            return View(listBill.OrderBy(x => x.ItemName));
+        }
+    }
+}
---
+++ PromotionEngineNetCore/Models/CheckoutModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionEngineNetCore.Models
+{
+    public class CheckoutModel
+    {
+        public string ItemName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int TotalQuantity { get; set; }
+    }
+}
